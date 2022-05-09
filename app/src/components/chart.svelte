f1cdaf05845595dba0ad2545d5148a07f6edf0fc
NanguRepo <nanguthenangu@gmail.com>
2022-05-09 18:57:34 +0200

    functioning graph

---
+++ app/src/components/chart.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Line from "svelte-chartjs/src/Line.svelte"
+	export let data: Object = {
+            labels: [...Array(10).keys()],
+            datasets: [
+                {
+                    label: "Value",
+                    backgroundColor: "rgba(255,99,132,0.2)",
+                    borderColor: "rgba(255,99,132,1)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)"
+                }
+            ]
+        };
+</script>
+
+<div>
+  	<Line bind:data={data}/>
+</div>
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,21 +1,61 @@
-<div class="bg-slate-200 shadow-lg rounded-lg px-4 py-4 w-fit h-auto m-auto mt-10 dark:bg-slate-700">
-    <input type="number" placeholder="num1" bind:value={x} />
-    <input type="number" placeholder="num2" bind:value={y} />
-    <button 
-        class="rounded bg-slate-300 px-2 hover:bg-slate-400"
-        on:click={handleClick}
-    >find out</button>
-    <h1 class="text-2xl break-words">{output}</h1>
-</div>
 
 <script lang="ts">
-    const isDivisible = (a: number, b: number) => {
-        return a % b === 0;
+	import { fade } from 'svelte/transition';
+    import Chart from "../components/chart.svelte"
+    const collatz = (n: number) => {
+        // create a new array with every value in the collatz conjecture with n as the seed
+        const arr = [n]
+        while (n > 1) {
+            n = n % 2 === 0 ? n / 2 : 3 * n + 1;
+            arr.push(n)
+        }
+        return arr
     }
     let x: number = 0;
-    let y: number = 0;
-    let output: boolean;
+    let data: Array<number>;
+    let chartData: Object;
     const handleClick = () => {
-        output = isDivisible(x, y);
+        data = collatz(x)
+        chartData = {
+            labels: [...Array(data.length).keys()],
+            datasets: [
+                {
+                    label: "Value",
+                    backgroundColor: "rgba(255,99,132,0.2)",
+                    borderColor: "rgba(255,99,132,1)",
+                    borderWidth: 1,
+                    hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                    hoverBorderColor: "rgba(255,99,132,1)",
+                    data: data
+                }
+            ], 
+        }
     }
+    
 </script>
+
+<div class="bg-slate-200 shadow-lg rounded-lg px-4 py-4 w-4/5 m-auto mt-10 dark:bg-slate-700">
+    <div class="pb-10">
+        <label for="x">Enter a number:</label>
+        <input type="number" class="bg-slate-100 dark:bg-slate-600 rounded ml-1" placeholder="number" bind:value={x} on:change={handleClick} />
+        <button 
+            class="rounded bg-slate-300 px-2 hover:bg-slate-400"
+            on:click={handleClick}
+        >
+            Calculate!
+        </button>
+        <button 
+            class="rounded bg-slate-300 px-2 hover:bg-slate-400"
+            on:click={() => {
+                // generate a new random number
+                x = Math.floor(Math.random() * 10000)
+                handleClick()
+            }}
+        >
+            Calculate from random seed
+        </button>
+    </div>
+    <div transition:fade class="outline-dotted rounded">
+        <Chart data={chartData}/>
+    </div>
+</div>
