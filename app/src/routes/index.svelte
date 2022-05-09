aa98ce9fc4e4b801bc001aebc475901d6822eb3b
NanguRepo <nanguthenangu@gmail.com>
2022-05-09 09:59:20 +0200

    skelet

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-slate-200 shadow-lg rounded-lg px-4 py-4 w-fit h-auto m-auto mt-10 dark:bg-slate-700">
+    <input type="number" placeholder="num1" bind:value={x} />
+    <input type="number" placeholder="num2" bind:value={y} />
+    <button 
+        class="rounded bg-slate-300 px-2 hover:bg-slate-400"
+        on:click={handleClick}
+    >find out</button>
+    <h1 class="text-2xl break-words">{output}</h1>
+</div>
+
+<script lang="ts">
+    const isDivisible = (a: number, b: number) => {
+        return a % b === 0;
+    }
+    let x: number = 0;
+    let y: number = 0;
+    let output: boolean;
+    const handleClick = () => {
+        output = isDivisible(x, y);
+    }
+</script>
