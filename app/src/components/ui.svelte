abd5e71ac48a0fb47f9f2a7fd59d2c22fe8958c3
NanguRepo <nanguthenangu@gmail.com>
2022-05-10 09:38:07 +0200

    dakr mode functionality

---
+++ app/src/components/ui.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import Chart from "../components/chart.svelte"
+    const collatz = (n: number) => {
+        // create a new array with every value in the collatz conjecture with n as the seed
+        const arr = [n]
+        while (n > 1) {
+            n = n % 2 === 0 ? n / 2 : 3 * n + 1;
+            arr.push(n)
+        }
+        return arr
+    }
+    let x: number;
+    let chartData: Object;
+    $: data = collatz(x);
+    $: chartData = {
+        labels: [...Array(data.length).keys()],
+        datasets: [
+            {
+                label: "Value",
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                borderWidth: 1,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: data
+            }
+        ], 
+    }
+    
+</script>
+
+<div class="bg-slate-200 shadow-lg rounded-lg px-4 py-4 w-4/5 m-auto mt-10 dark:bg-slate-700">
+    <div class="pb-10">
+        <input type="number" class="bg-slate-100 dark:bg-slate-600 rounded ml-1 py-2 px-4" placeholder="Any positive number" bind:value={x} />
+        <button 
+            class="rounded bg-slate-300 hover:bg-slate-400 dark:bg-slate-500 dark:hover:bg-slate-500/75 py-2 px-4"
+            on:click={() => {
+                // generate a new random number
+                x = Math.floor(Math.random() * 10000)
+            }}
+        >
+            Get a random number
+        </button>
+    </div>
+    <div class="rounded bg-gray-100/50 dark:bg-gray-800/50">
+        <Chart data={chartData}/>
+    </div>
+</div>
--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,57 +1,16 @@
 <script context="module">
 	export const prerender = true;
+    import UI from "../components/ui.svelte";
 </script>
 
 <svelte:head>
     <title>Collatz Conjecture Visualizer</title>
 </svelte:head>
 
-<script lang="ts">
-    import Chart from "../components/chart.svelte"
-    const collatz = (n: number) => {
-        // create a new array with every value in the collatz conjecture with n as the seed
-        const arr = [n]
-        while (n > 1) {
-            n = n % 2 === 0 ? n / 2 : 3 * n + 1;
-            arr.push(n)
-        }
-        return arr
-    }
-    let x: number;
-    let chartData: Object;
-    $: data = collatz(x);
-    $: chartData = {
-        labels: [...Array(data.length).keys()],
-        datasets: [
-            {
-                label: "Value",
-                backgroundColor: "rgba(255,99,132,0.2)",
-                borderColor: "rgba(255,99,132,1)",
-                borderWidth: 1,
-                hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                hoverBorderColor: "rgba(255,99,132,1)",
-                data: data
-            }
-        ], 
-    }
-    
-</script>
-
-<div class="bg-slate-200 shadow-lg rounded-lg px-4 py-4 w-4/5 m-auto mt-10 dark:bg-slate-700">
-    <div class="pb-10">
-        <label for="x">Enter a number:</label>
-        <input type="number" class="bg-slate-100 dark:bg-slate-600 rounded ml-1" placeholder="Any positive number" bind:value={x} />
-        <button 
-            class="rounded bg-slate-300 px-2 hover:bg-slate-400"
-            on:click={() => {
-                // generate a new random number
-                x = Math.floor(Math.random() * 10000)
-            }}
-        >
-            Get a random number
-        </button>
+<body class="dark:bg-gray-800 dark:text-white">
+    <div class="px-4 py-4 w-4/5 m-auto mt-10 text-center">
+        <h1 class="text-7xl font-bold">Collatz Conjecture Visualizer</h1>
+        <h2 class="text-2xl text-gray-600 dark:text-gray-400">Put in a number. We'll halve it if it's even, and triple it and add one if it's odd. Keep doing that, and it'll <a class="text-blue-600 dark:text-blue-300 hover:underline hover:animate-pulse" href="https://en.wikipedia.org/wiki/Collatz_conjecture" target="_blank">eventually become one</a>. This is plotted to a graph in order to visualize.</h2>
     </div>
-    <div class="outline-dotted rounded">
-        <Chart data={chartData}/>
-    </div>
-</div>
+    <UI />
+</body>
